7c2d1ea513ab9d7f1a54a661590647dacbc23741
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-06 08:41:50 -0700

    add a failing second test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,9 +2,13 @@ require('pry')
 require('rspec')
 require('anagram')
 
-describe('#anagram_check') do 
+describe('#is_anagram') do 
   it('should know if two words are anagrams') do
     string = Anagram.new('now', 'won')
+    expect(string.is_anagram).to(eq('These words are anagrams'))
+  end
+  it('should be not case sensitive') do
+    string = Anagram.new('Now', 'Won')
     expect(string.is_anagram).to(eq('These words are anagrams'))
   end
 end
