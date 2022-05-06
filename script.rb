fb2863273b78e816c3e062108f3a848f4b094598
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-06 15:04:42 -0700

    update script.rb to respond to new code

--- script.rb
+++ script.rb
@@ -7,8 +7,9 @@ input1 = gets.chomp
 puts 'Please enter your second word or sentence'
 input2 = gets.chomp
 check = Anagram.new(input1, input2)
+output = check.is_anagram
 
-if check.is_anagram == 'These words are not anagrams'
+if output.include? 'These words are not anagrams'
   puts <<-'HEREDOC'
     .-"-.
   _/.-.-.\_
@@ -18,7 +19,7 @@ if check.is_anagram == 'These words are not anagrams'
    /`"""`\
   /       \
   HEREDOC
-elsif check.is_anagram == 'Invalid input. Words must contain vowels.'
+elsif output == 'Invalid input. Words must contain vowels.'
   puts <<-'HEREDOC'
    ,-.       _,---._ __  / \
   /  )    .-'       `./ /   \
@@ -32,7 +33,7 @@ elsif check.is_anagram == 'Invalid input. Words must contain vowels.'
    )  |  \  `.___________|/
    `--'   `--'
   HEREDOC
-elsif check.is_anagram == 'No letters in these words match, making them antigrams'
+elsif output == 'No letters in these words match, making them antigrams'
   puts <<-'HEREDOC'
         __  _
     .-.'  `; `-._  __  _
@@ -55,4 +56,4 @@ else
 ~^~^~^~^~^~^~^~^~^~^~^~^~
   HEREDOC
 end
-puts check.is_anagram()
+puts output
