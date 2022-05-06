08771ffd291bd3440fd30026c7bfda73c3777a1f
Gabe Nielsen <gabenielsen7@gmail.com>
2022-05-06 08:50:13 -0700

    add a failing third test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -11,4 +11,8 @@ describe('#is_anagram') do
     string = Anagram.new('Now', 'Won')
     expect(string.is_anagram).to(eq('These words are anagrams'))
   end
+  it('should make sure the words contain a vowel') do
+    string = Anagram.new('hjkl', 'lkjh')
+    expect(string.is_anagram).to(eq("Invalid input. These words muxt contain vowels: 'hjkl', 'lkjh'"))
+  end
 end
